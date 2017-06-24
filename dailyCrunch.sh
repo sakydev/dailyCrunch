d669b5def8ff73bc7e879851cb03c6dcf04e9eda
Saqib Razzaq <saqi.cb@gmail.com>
2017-06-24 19:55:02 +0500

    Added functionality to take backups and synch servers

--- dailyCrunch.sh
+++ dailyCrunch.sh
@@ -5,6 +5,7 @@ case "$1" in
 # Count number of lines matches
 help) 
   echo "List of paramters: "
+  echo "command: svnupdate file_path.txt : file_format: server_user_name|server_ip|server_pass|directory_to_update"
   ;;
 svnupdate)
   echo "Bulk svn update of servers in file:" $2
@@ -14,9 +15,13 @@ svnupdate)
     server_pasword=$(echo $line| cut -d'|' -f 3)
     server_directory=$(echo $line| cut -d'|' -f 4)
     echo "Porcessing server :" $server_ip "as user :" $server_user "to update directory :" $server_directory
-    sshpass -p $server_pasword server_user@server_ip svn update $server_directory
+    sshpass -p $server_pasword $server_user@$server_ip svn update $server_directory
 
   done < "$2"
+  ;;
+hardstart)
+  echo "Restarting Apache & MySQL"
+  service apache2 restart && service mysql restart
   ;;
 phpproc) 
   echo "List of active PHP processes"
@@ -37,4 +42,20 @@ proc)
 dirsize)
   echo "Biggest $3 files in $2 directory"
   du -a $2 | sort -n -r | head -n $3
+  ;;
+syncservers)
+  echo "Backing up and synching directory: " $3 " with servers in file:" $2
+  backup_directory=$4
+  directory_to_sync=$3
+  zip -r 
+  while IFS='' read -r line || [[ -n "$line" ]]; do
+    server_user=$(echo $line| cut -d'|' -f 1)
+    server_ip=$(echo $line| cut -d'|' -f 2)
+    server_pasword=$(echo $line| cut -d'|' -f 3)
+    remote_server_directory=$(echo $line| cut -d'|' -f 4)
+    echo "Porcessing server :" $server_ip "as user :" $server_user "to synch directory :" $remote_server_directory
+    sshpass -p $server_pasword $server_user@$server_ip rm -rf $remote_server_directory
+    sshpass -p $server_pasword scp -r $directory_to_sync $server_user@$server_ip:$remote_server_directory
+  done < "$2"
+  ;;
 esac
