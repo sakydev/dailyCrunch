845cfddc891bbb8a6b630bbbdd9d7250f862f121
Saqib Razzaq <saqi.cb@gmail.com>
2017-06-24 21:13:21 +0500

    Added webcapture

--- dailyCrunch.sh
+++ dailyCrunch.sh
@@ -19,6 +19,16 @@ svnupdate)
 
   done < "$2"
   ;;
+golunch)
+  echo "Running for lunch..."
+  echo "Killing Skype"
+  pkill skype
+  echo "Killing Submile Text"
+  pkill sublime
+  echo "Killing Chrome"
+  pkill chrome
+  gnome-screensaver-command -l
+  ;;
 blast)
   echo "Play all mp3 tracks in directory " $2
   if [ -z "$2" ]; then
@@ -39,6 +49,32 @@ ytaudio)
     fi
     echo "Saving audio to directory: " $output_directory
     youtube-dl --extract-audio --audio-format mp3 --audio-quality 0 -o "$output_directory/%(title)s.%(ext)s" $2  
+  fi
+  ;;
+webcapture)
+  if [ -z "$2" ]; then
+    echo "Website URL must be provided"
+  else
+    capture_url=$2
+    if [ -z "$3" ]; then
+      output_path=$PWD/'screenshot.png'
+    fi
+
+    if [ -z "$4" ]; then
+      screenshot_widh='1280'
+    else
+      screenshot_widh=$4
+    fi
+
+    if [ -z "$5" ]; then
+      screenshot_height='720'
+    else
+      screenshot_height=$5
+    fi
+
+
+    echo "Capturing $capture_url and saving to $output_path"
+    phantomjs path_to_script.js $capture_url $output_path $screenshot_widh $screenshot_height
   fi
   ;;
 countfiles)
